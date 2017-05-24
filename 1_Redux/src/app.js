4fb677ab2368ef022ea71c49cde522b2d3fae3f4
you <you@example.com>
2017-05-24 20:45:38 -0300

    CRUD Redux: books+cart

--- 1_Redux/src/app.js
+++ 1_Redux/src/app.js
@@ -1,57 +1,39 @@
 "use strict"
 import {createStore} from 'redux';
+import reducers from './reducers/index'
+import {addToCart} from './actions/cartActions';
+import {postBook, deleteBook, updateBook} from './actions/bookActions';
 
-const reducer = function(state={books: []}, action) {
-  switch (action.type) {
-    /*case 'INCREMENT':
-      return state + action.payload;
-      break;
-    case 'DECREMENT':
-      return state - action.payload;
-      break;*/
-    case 'POST_BOOK':
-      //let books = state.books.concat(action.payload);
-      //return {books}
-      return {books: [...state.books, ...action.payload]}
-      break;
-  }
-
-  return state;
-}
-
-const store = createStore(reducer);
+const store = createStore(reducers);
 
 store.subscribe(function() {
   console.log('current state is ', store.getState());
-  //console.log('current price ', store.getState()[1].price);
 });
 
-//store.dispatch({type: 'INCREMENT', payload: 1});
-//store.dispatch({type: 'INCREMENT', payload: 1});
-//store.dispatch({type: 'INCREMENT', payload: 2});
-//store.dispatch({type: 'DECREMENT', payload: 1});
-store.dispatch({
-  type: 'POST_BOOK',
-  payload: [{
-    id: 1,
-    title: 'Java programming',
-    description: 'libro malo',
-    price: 35.50
-  },
+store.dispatch(postBook(
+    [
+      {
+        id: 1,
+        title: 'Java programming',
+        description: 'libro malo',
+        price: 35.50
+      },
+      {
+        id: 2,
+        title: 'Python OO 2016',
+        description: 'libro excelente',
+        price: 42.80
+      }
+    ]
+));
+
+store.dispatch(deleteBook({id: 1}));
+store.dispatch(updateBook(
   {
     id: 2,
-    title: 'Python OO 2016',
-    description: 'libro excelente',
-    price: 42.80
-  }]
-});
+    title: 'Python para todos',
+    description: 'Libro para novatos'
+  }
+));
 
-store.dispatch({
-  type: 'POST_BOOK',
-  payload: [{
-    id: 3,
-    title: 'PHP5 POO',
-    description: 'enrique place',
-    price: 42.00
-  }]
-});
+store.dispatch(addToCart([{id: 45}]));
