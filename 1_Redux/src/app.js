d9e4345acf55c5010832b4504ec06cfef0444d88
you <you@example.com>
2017-05-24 12:47:13 -0300

    CRUD Redux

--- 1_Redux/src/app.js
+++ 1_Redux/src/app.js
@@ -1,7 +1,7 @@
 "use strict"
 import {createStore} from 'redux';
 
-const reducer = function(state={}, action) {
+const reducer = function(state={books: []}, action) {
   switch (action.type) {
     /*case 'INCREMENT':
       return state + action.payload;
@@ -10,7 +10,9 @@ const reducer = function(state={}, action) {
       return state - action.payload;
       break;*/
     case 'POST_BOOK':
-      return state = action.payload;
+      //let books = state.books.concat(action.payload);
+      //return {books}
+      return {books: [...state.books, ...action.payload]}
       break;
   }
 
@@ -21,7 +23,7 @@ const store = createStore(reducer);
 
 store.subscribe(function() {
   console.log('current state is ', store.getState());
-  console.log('current price ', store.getState().price);
+  //console.log('current price ', store.getState()[1].price);
 });
 
 //store.dispatch({type: 'INCREMENT', payload: 1});
@@ -30,10 +32,26 @@ store.subscribe(function() {
 //store.dispatch({type: 'DECREMENT', payload: 1});
 store.dispatch({
   type: 'POST_BOOK',
-  payload: {
+  payload: [{
     id: 1,
     title: 'Java programming',
     description: 'libro malo',
     price: 35.50
-  }
+  },
+  {
+    id: 2,
+    title: 'Python OO 2016',
+    description: 'libro excelente',
+    price: 42.80
+  }]
+});
+
+store.dispatch({
+  type: 'POST_BOOK',
+  payload: [{
+    id: 3,
+    title: 'PHP5 POO',
+    description: 'enrique place',
+    price: 42.00
+  }]
 });
